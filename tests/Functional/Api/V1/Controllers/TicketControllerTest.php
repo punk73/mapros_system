4434af49d826fc0818e914bf2fabeea4a6900f5d
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-27 16:56:57 +0700

    finish testing ticket

---
+++ tests/Functional/Api/V1/Controllers/TicketControllerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Functional\Api\V1\Controllers;
+
+use App\TestCase;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Functional\Api\V1\Traits\testHelper;
+
+class TicketControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $endpoint = 'api/tickets/';
+
+    public function __construct(){
+        $this->model = new Ticket;
+    }
+
+    protected $expectedJsonStructure = [
+        'data',
+    ];
+
+    protected $inputParameter = [
+        'code' => 'PLN',
+        'line_id' => 1,
+        'count' => 1
+    ];
+
+    protected $failedInputParameter = [
+        'notName' => 'klasdf'
+    ];
+
+    public function testPostSuccess (){
+        $result = $this->post($this->endpoint, $this->inputParameter );
+        $result->assertJsonStructure([
+            'success',
+            'data'
+        ])->assertJson([
+            'success' => true
+        ]);
+    }
+
+    public function testReturnValueCorrect(){
+        $this->inputParameter['count'] = 5;
+        $result = $this->post($this->endpoint, $this->inputParameter );
+        $returnValue = $result->original;
+        $generatedCode = 'PLN00100001';
+        // fwrite(STDOUT, var_dump( $returnValue ) );
+        $this->assertEquals(count($returnValue['data']), ($this->inputParameter['count']+1) );
+        $this->assertEquals( $returnValue['data'][0], $generatedCode );
+    }
+
+
+}
