27a49fd7d34482562709acc2e52e6c548ae15ba1
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-27 11:51:03 +0700

    add connection seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ScannerSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ConnectionSeeder::class);
+
     }
 }
