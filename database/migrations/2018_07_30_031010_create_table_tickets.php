807c871999d08e0e4d16b588f339dd51abf35a0a
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-07-30 10:15:31 +0700

    add table & model Ticket

---
+++ database/migrations/2018_07_30_031010_create_table_tickets.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ticket_no');
+            $table->string('guid_master')->nullable();
+            $table->string('guid_ticket')->nullable();
+            $table->integer('scanner_id');
+            $table->string('status');
+            $table->string('scan_nik');
+            $table->string('judge')->default('OK');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
